e38199ac4735bae1253626aacfed553b613bc073
Serhii Malychkovych <masevik@gmail.com>
2022-09-16 09:19:08 +0300

    Task 2 done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,22 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galleryRef = document.querySelector('.gallery');
+const markup = createMarkup();
+
+function createMarkup() {
+  return galleryItems.map(({ preview, original, description }) => {
+    return `<a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+  </a>`;
+  });
+}
+
+galleryRef.insertAdjacentHTML('beforeend', markup.join(' '));
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionsData: 'alt',
+});
